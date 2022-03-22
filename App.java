042ce4ae4931c69180d4b9e675bd79c701d27bc4
Pedram Nouri <noori.pedram816@gmail.com>
2022-03-22 18:56:35 -0400

    add dummy method

--- App.java
+++ App.java
@@ -1,23 +1,36 @@
-package org.example;
+package com.company;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Test {
+    private int a;
+    private int b;
+
+    public Test(int a, int b) {
+        this.a = a;
+        this.b = b;
     }
 
-    public void againChanged() {
-        String name = "Ahmad";
+    public int getA() {
+        return a;
+    }
 
-        if (name.contains("Reza")) {
-            float f = 4.2221;
-        }
+    public void setA(int a) {
+        this.a = a;
+    }
 
-        print("f not finede");
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public void calc() {
+        int c = 2 * this.a + this.b;
+
+        if (c > 10)
+            System.out.println("Nice!");
+        else
+            System.out.println("NOOO");
     }
 }
