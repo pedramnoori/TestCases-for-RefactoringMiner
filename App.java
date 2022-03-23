b5a91a587d69fed983192921a7b3c1a5c47df504
Pedram Nouri <noori.pedram816@gmail.com>
2022-03-23 16:41:48 -0400

    add code

--- App.java
+++ App.java
@@ -1,13 +1,15 @@
 void printOwing() {
-        Enumeration e = _orders.elements(); double outstanding = 0.0;
-        printBanner();
-// calculate outstanding while (e.hasMoreElements()) {
-        Order each = (Order) e.nextElement();
-        outstanding += each.getAmount(); }
-        printDetails(outstanding);
-        }
+    Enumeration e = _orders.elements();
+    double outstanding = 0.0;
+    System.out.println ("**************************");
+    System.out.println ("***** Customer Owes ******");
+    System.out.println ("**************************");
 
-        void printDetails (double outstanding) {
+    while (e.hasMoreElements()) {
+        Order each = (Order) e.nextElement();
+        outstanding += each.getAmount();
+    }
+
     System.out.println ("name:" + _name);
     System.out.println ("amount" + outstanding);
-        }
+}
