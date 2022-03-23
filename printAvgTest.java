c216c80e012940f5db6fa3abac785d1fc483a058
Pedram Nouri <noori.pedram816@gmail.com>
2022-03-23 17:02:23 -0400

    new added file

---
+++ printAvgTest.java
@@ -0,0 +1,10 @@
+package printAvg;
+public class printAvgTest {
+    public static void printAvg(int sum, int count){
+        System.out.println(sum / count);
+    }
+    public static void main(String[] args) {
+        printAvg(10,2);
+    }
+
+}
