3150eb9894a4a0e719cd1b033b82d154dfc63b2c
Pedram Nouri <noori.pedram816@gmail.com>
2022-03-23 17:33:07 -0400

    added

--- RefacTest/src/com/company/Main.java
+++ RefacTest/src/com/company/Main.java
@@ -4,4 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
     }
+
+    void printOwing() {
+
+        Enumeration e = _orders.elements();
+        double outstanding = 0.0;
+
+        System.out.println ("**************************");
+        System.out.println ("***** Customer Owes ******");
+        System.out.println ("**************************");
+
+        while (e.hasMoreElements()) {
+            Order each = (Order) e.nextElement();
+            outstanding += each.getAmount();
+        }
+
+        System.out.println ("name:" + _name);
+        System.out.println ("amount" + outstanding);
+    }
 }
