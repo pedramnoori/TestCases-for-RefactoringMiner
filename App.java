b8de1106b5bb0eae74a1b7d7546137f965b8313d
Pedram Nouri <noori.pedram816@gmail.com>
2022-03-23 16:47:11 -0400

    tee

--- App.java
+++ App.java
@@ -2,9 +2,7 @@ public class App {
     void printOwing() {
         Enumeration e = _orders.elements();
         double outstanding = 0.0;
-        System.out.println ("**************************");
-        System.out.println ("***** Customer Owes ******");
-        System.out.println ("**************************");
+        extracted();
 
         while (e.hasMoreElements()) {
             Order each = (Order) e.nextElement();
@@ -13,5 +11,11 @@ public class App {
 
         System.out.println ("name:" + _name);
         System.out.println ("amount" + outstanding);
+    }
+
+    private void extracted() {
+        System.out.println ("**************************");
+        System.out.println ("***** Customer Owes ******");
+        System.out.println ("**************************");
     }
 }
