c4c1ae7afedf512fca4c5c8f42566232b594ff83
Pedram Nouri <noori.pedram816@gmail.com>
2022-03-23 17:44:53 -0400

    added

--- RefacTest/src/com/company/Main.java
+++ RefacTest/src/com/company/Main.java
@@ -21,7 +21,19 @@ public class Main {
         System.out.println ("amount" + outstanding);
     }
 
-    private void extractedMethodTwo() {
+    private void extractedMethod() {
         System.out.println ("**************************");
+        System.out.println ("***** Customer Owes ******");
+        System.out.println ("**************************");
+        System.out.println ("--------------------------");
+        System.out.println ("--------------------------");
+        System.out.println ("++++++++++++++++++++++++++");
+        System.out.println ("++++++++++++++++++++++++++");
+        System.out.println ("++++++++++++++++++++++++++");
+        System.out.println ("++++++++++++++++++++++++++");
+        System.out.println ("++++++++++++++++++++++++++");
+        System.out.println ("++++++++++++++++++++++++++");
+        System.out.println ("++++++++++++++++++++++++++");
+        System.out.println ("++++++++++++++++++++++++++");
     }
 }
