848e5e09e76a22874af1623d144b932140025243
Pedram Nouri <noori.pedram816@gmail.com>
2022-03-23 16:43:06 -0400

    extract banner

--- App.java
+++ App.java
@@ -1,9 +1,7 @@
 void printOwing() {
     Enumeration e = _orders.elements();
     double outstanding = 0.0;
-    System.out.println ("**************************");
-    System.out.println ("***** Customer Owes ******");
-    System.out.println ("**************************");
+    printBanner();
 
     while (e.hasMoreElements()) {
         Order each = (Order) e.nextElement();
@@ -12,4 +10,10 @@ void printOwing() {
 
     System.out.println ("name:" + _name);
     System.out.println ("amount" + outstanding);
+}
+
+void printBanner() {
+    System.out.println ("**************************");
+    System.out.println ("***** Customer Owes ******");
+    System.out.println ("**************************");
 }
