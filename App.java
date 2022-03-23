1260c7f96255aaf3bc2d0c54ee76ec4511afac20
Pedram Nouri <noori.pedram816@gmail.com>
2022-03-23 16:55:25 -0400

    add pack

--- App.java
+++ App.java
@@ -1,8 +1,12 @@
+package com.company;
+
 public class App {
     void printOwing() {
         Enumeration e = _orders.elements();
         double outstanding = 0.0;
-        extracted();
+        System.out.println ("**************************");
+        System.out.println ("***** Customer Owes ******");
+        System.out.println ("**************************");
 
         while (e.hasMoreElements()) {
             Order each = (Order) e.nextElement();
@@ -11,11 +15,5 @@ public class App {
 
         System.out.println ("name:" + _name);
         System.out.println ("amount" + outstanding);
-    }
-
-    private void extracted() {
-        System.out.println ("**************************");
-        System.out.println ("***** Customer Owes ******");
-        System.out.println ("**************************");
     }
 }
