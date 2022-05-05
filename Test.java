73a2e9f33a46dc649f36e55d4db6a90c55ec565d
Pedram Nouri <noori.pedram816@gmail.com>
2022-05-04 20:25:27 -0400

    t2

--- Test.java
+++ Test.java
@@ -5,8 +5,17 @@ public class Test {
     void printOwing(int t1, int t2) {
 
         printBanner();
-        int t3 = (t1 + t2) * 4;
-        System.out.println("The expected value would be " + t3);
+        int t3 = calValue(t1, t2);
+
+        System.out.println("The expected value is " + t3);
+
+    }
+
+    int calValue(int a, int b){
+      
+      int c = (a + b) * 4;
+      System.out.println("dummy");
+      return c;
 
     }
 }
