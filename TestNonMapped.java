5010b90c9b489abbb1be58a58b6deb8c0ac50742
unknown <noori.pedram816@gmail.com>
2022-06-01 22:32:40 -0500

    add new test

---
+++ TestNonMapped.java
@@ -0,0 +1,14 @@
+package com.facebook.buck.cli;
+
+import list;
+
+public class TestNonMapped {
+
+    void printOwing(int t1) {
+
+        printBanner();
+        int t3 = t1 * 4;
+        System.out.println("The expected value would be " + t3);
+
+    }
+}
