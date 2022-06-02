661d1cee4cfeb521dacbc991abf31842caed8b62
unknown <noori.pedram816@gmail.com>
2022-06-01 22:33:52 -0500

    extract method - add return statement

--- TestNonMapped.java
+++ TestNonMapped.java
@@ -1,14 +1,19 @@
 package com.facebook.buck.cli;
 
-import list;
-
 public class TestNonMapped {
 
     void printOwing(int t1) {
 
         printBanner();
-        int t3 = t1 * 4;
+        int t3 = calValue(t1);
+
         System.out.println("The expected value would be " + t3);
 
+    }
+
+    int calValue(int t1){
+      
+      int t3 = t1 * 4;
+      return t3;
     }
 }
