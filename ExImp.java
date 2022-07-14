e803f6cf44aa9695f2022f36b5b74c0f6d682419
Pedram Nouri <noori.pedram816@gmail.com>
2022-07-14 09:43:08 -0400

    impure extract added

---
+++ ExImp.java
@@ -0,0 +1,25 @@
+package com.facebook.buck.cli;
+
+import list;
+
+public class Test {
+
+    void printOwing(int t1) {
+
+        printBanner(t1);
+        t1 = t1 * 4;
+
+        System.out.println("The expected value would be " + t3);
+
+    }
+    int printBanner(int t1) {
+
+    if (t1 > 10) 
+        t1 = doA(t1);
+    else
+        t1 =  doB(t1);
+    
+    return t1;
+    }
+}
+
