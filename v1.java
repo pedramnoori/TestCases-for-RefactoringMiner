44dbcbe22cf42e0cb76890438524ab72004e3025
Pedram Nouri <noori.pedram816@gmail.com>
2022-09-09 03:36:05 -0400

    v1

--- v1.java
+++ v1.java
@@ -1,32 +1,6 @@
 public class ChunckOptionPopupPanel {
 
-
-private boolean has(String key)
-   {
-      return chunkOptions_.containsKey(key);
-   }
-
-   public String get(String key)
-   {
-      return chunkOptions_.get(key);
-   }
-
-   private boolean getBoolean(String key)
-   {
-      return isTrue(chunkOptions_.get(key));
-   }
-
-   private void set(String key, String value)
-   {
-      chunkOptions_.put(key,  value);
-   }
-
-   private void unset(String key)
-   {
-      chunkOptions_.remove(key);
-   }
-
-   public void init(AceEditorWidget widget, Position position)
+public void init(AceEditorWidget widget, Position position)
    {
       widget_ = widget;
       position_ = position;
@@ -35,15 +9,20 @@ private boolean has(String key)
       originalLine_ = widget_.getEditor().getSession().getLine(position_.getRow());
       parseChunkHeader(originalLine_, chunkOptions_);
 
-      for (Map.Entry<String, String> pair : originalChunkOptions_.entrySet())
-        {
-        boolean truthy = false;
-
-        if (has("fig.width")) {
-            truthy = getBoolean("fig.width");
-        }
-        if (has("fig.height")) {
-            truthy = getBoolean("fig.height")
-        }
+      for (String option : BOOLEAN_CHUNK_OPTIONS.keySet())
+      {
+         if (chunkOptions_.containsKey(option))
+         {
+            boolean truthy = isTrue(chunkOptions_.get(option));
+            if (truthy)
+               cb.setState(TriStateCheckBox.STATE_ON);
+            else
+               cb.setState(TriStateCheckBox.STATE_OFF);
+         }
+         else
+         {
+            cb.setState(TriStateCheckBox.STATE_INDETERMINATE);
+         }
       }
+   }
 }
