872b4cd4450c76f8462f70e2e5e3b3419710af96
unknown <noori.pedram816@gmail.com>
2023-07-22 17:45:28 -0500

    test

--- InVar.java
+++ InVar.java
@@ -3,14 +3,11 @@ class InVar {
   private void post(HttpResponseStatus responseStatus) {
 
     doSomething();
-
-    extractedMethod(responseStatus);
-  }
-
-  private void extractedMethod(HttpResponseStatus responseStatus) {
+    
     if (channel.isOpen()) {
-      if (!isKeepAlive) {a
-         channel.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT).addListener(ChannelFutureListener.CLOSE);
+      ChannelFuture lastContentFuture = channel.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT);
+      if (!isKeepAlive) {
+        lastContentFuture.addListener(ChannelFutureListener.CLOSE);
       }
     }
   }
