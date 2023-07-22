26926b887ce7e38603f052daa0d894cb1dafa857
unknown <noori.pedram816@gmail.com>
2023-07-22 17:33:48 -0500

    Inline Variable on top of Extract Method

---
+++ InVar.java
@@ -0,0 +1,14 @@
+Class Bar {
+
+  private void post(HttpResponseStatus responseStatus) {
+
+    doSomething();
+    
+    if (channel.isOpen()) {
+      ChannelFuture lastContentFuture = channel.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT);
+      if (!isKeepAlive) {
+        lastContentFuture.addListener(ChannelFutureListener.CLOSE);
+      }
+    }
+  }
+}
