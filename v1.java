c83e4c00f0d36fb2e3b1e7ad752c499cbefd9481
Pedram Nouri <noori.pedram816@gmail.com>
2023-10-18 18:56:25 -0400

    test2

--- v1.java
+++ v1.java
@@ -1,5 +1,24 @@
-public class NIOServerCnxn extends ServerCnxn { 
-    private void cleanupWriterSocket(PrintWriter pwriter) {
+public abstract class AbstractFourLetterCommand {
+   protected ServerCnxn serverCnxn;
+
+   public void run() {
+      try {
+         commandRun();
+      } catch (IOException ie) {
+         LOG.error("Error in running command ", ie);
+      } finally {
+         serverCnxn.cleanupWriterSocket(pw);
+      }
+      extractedRun();
+   }
+   public void extractedRun() {
+        int a = 10;
+        int b = a + 25;
+        return;
+   }
+}
+public abstract class ServerCnxn {
+       public void cleanupWriterSocket(PrintWriter pwriter) {
         try {
             if (pwriter != null) {
                 pwriter.flush();
@@ -7,22 +26,6 @@ public class NIOServerCnxn extends ServerCnxn {
         } catch (Exception e) {
             LOG.info("Error closing PrintWriter ", e);
         }
-    } 
-    private abstract class CommandThread {
-        public void run() {
-            try {
-                commandRun();
-            } catch (IOException ie) {
-                LOG.error("Error in running command ", ie);
-            } finally {
-                cleanupWriterSocket(pw);
-            }
-        }
-        int a = 10;
-        int b = a + 25;
-        return;
     }
 }
-
-
 
